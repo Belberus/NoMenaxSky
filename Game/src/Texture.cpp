0e3358a0f1ca9704540a613096af0b28239b480d
Alberto Martinez Menendez <amartinez95zgz@gmail.com>
2017-03-29 18:58:41 +0200

    Fantasma añadido

--- Game/src/Texture.cpp
+++ Game/src/Texture.cpp
@@ -31,6 +31,7 @@ Texture &Texture::operator=(const Texture &rhs) {
 }
 
 void Texture::load() {
+  glBindTexture(GL_TEXTURE_2D, 0);
   glBindTexture(GL_TEXTURE_2D, id);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);

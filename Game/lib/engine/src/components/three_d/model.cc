0bcf6bff29ccc1ab9dbd5dc19ef62a711608c5ca
MIN605472 <MIN605472@users.noreply.github.com>
2017-08-25 13:21:52 +0200

    Fix - Forgot to add some files; fixes last commit.

---
+++ Game/lib/engine/src/components/three_d/model.cc
@@ -0,0 +1,9 @@
+#include "engine/components/three_d/model.h"
+
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <assimp/Importer.hpp>
+
+struct engine::components::three_d::Model::Impl {};
+
+engine::components::three_d::Model::Model(const std::string& filename) {}

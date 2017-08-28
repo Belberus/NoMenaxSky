3829f7be28649fff580826c272ca7d703d24a58d
Alberto Martinez Menendez <amartinez95zgz@gmail.com>
2017-08-28 17:47:27 +0200

    PlaySound en torreta arreglado

--- Game/src/systems.cc
+++ Game/src/systems.cc
@@ -687,7 +687,7 @@ void TurretAttackSystem::receive(const Collision &collision) {
   	proyectil.destroy();
 
     Engine::GetInstance().Get<AudioManager>().
-              PlaySound("assets/media/fx/talk.wav",false);
+              PlaySound("assets/media/fx/talk.wav",false,1);
     auto e1_color_animation = collision_copy.e1.component<ColorAnimation>();
     e1_color_animation->Play();
 
@@ -700,7 +700,7 @@ void TurretAttackSystem::receive(const Collision &collision) {
   	proyectil.destroy();
 
     Engine::GetInstance().Get<AudioManager>().
-              PlaySound("assets/media/fx/talk.wav",false);
+              PlaySound("assets/media/fx/talk.wav",false,1);
     auto e0_color_animation = collision_copy.e0.component<ColorAnimation>();
     e0_color_animation->Play();      
   } else {
